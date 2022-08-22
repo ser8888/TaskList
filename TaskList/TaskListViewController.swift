4b7b95b57d6cb567c6116db72cc389fb2a951ae1
Sergejs Tiselskis <slokas85@gmail.con>
2022-08-22 17:41:29 +0300

    interface with TableView is done.

--- TaskList/TaskListViewController.swift
+++ TaskList/TaskListViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class TaskListViewController: UITableViewController {
-
+    
+    private let cellId = "task"     //ячейка
+                    //создаем массив чтобы его отобразить в таблице
+    private var taskList: [Task] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        //регистрируем ячейку таблицы
+        //UItableView.self - это означает тип данных для ячейки
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         view.backgroundColor = .white
         setupNavigationBar()
         
@@ -59,3 +65,18 @@ class TaskListViewController: UITableViewController {
 
 }
 
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+}
